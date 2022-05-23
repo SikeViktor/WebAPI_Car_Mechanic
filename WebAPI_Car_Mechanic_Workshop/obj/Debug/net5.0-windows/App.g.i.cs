e53f775c4fe868ee6ec659ca51d01e57629d2706
SikeViktor <sikevityu@gmail.com>
2022-05-23 20:03:43 +0200

    SolutionRepair

--- WebAPI_Car_Mechanic_Workshop/obj/Debug/net5.0-windows/App.g.i.cs
+++ WebAPI_Car_Mechanic_Workshop/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace WebAPI_Car_Mechanic_Workshop {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace WebAPI_Car_Mechanic_Workshop {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public static void Main() {
             WebAPI_Car_Mechanic_Workshop.App app = new WebAPI_Car_Mechanic_Workshop.App();
             app.InitializeComponent();
